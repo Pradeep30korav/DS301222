4350c4b0aa7f6798a20042b28b3fab454a6b8e7a
Pradeep30korav <121960614+Pradeep30korav@users.noreply.github.com>
2023-01-13 12:43:05 +0530

    Update _8_even_odd.py

--- Python/_3_conditional_statements/_8_even_odd.py
+++ Python/_3_conditional_statements/_8_even_odd.py
@@ -1,6 +1,10 @@
 num = int(input("enter the number : "))
 
-if num%2==0:
+if num % 2==0:
     print("entered no is even value  : ", num)
 else:
     print("entered no is odd value : " , num)
+
+
+
+
